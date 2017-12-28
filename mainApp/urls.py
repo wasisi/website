9990bd70e299cb52de7750e064acc5a86e009896
Benedict Omare <benaboki@gmail.com>
2017-12-28 21:30:40 +0100

    Restore site to original, create new branch with about page and contact form

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
 	url(r'^$',
 			views.index,
 			name='index'),
-
+	url(r'^about/',
+			views.about, 
+			name='about'),
+	url(r'^contact/',
+			views.contact, 
+			name='contact')
 ]
