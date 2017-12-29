cdf5bd51b4279295684ddb4b4e765470eda9fa42
Benedict Omare <benaboki@gmail.com>
2017-12-29 21:40:54 +0100

    Add blog templates and blog to navigation

--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,16 @@ from .models import Post
 def post_list(request):
     posts = Post.published.all() # uses the manager that was created in models
     return render(request,
-                 'blog/post/list.html',
+                 'blog/list.html',
                  {'posts': posts})
 
 # Second view to display single post
 def post_detail(request, year, month, day, post):
-	post = get_object_or_404(Post, slug =post,
+	post = get_object_or_404(post, slug =post,
 									status =published,
 									publish__year=year,
 									publish__month=month,
 									publish__day=day)
 	return render(request,
-				'blog/post/detail.html',
+				'blog/detail.html',
 				{'post': post})
