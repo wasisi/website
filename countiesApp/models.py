7ff5b2b13ca57ebf3c0806822f78e900dfffb099
Benedict Omare <benaboki@gmail.com>
2018-01-14 21:26:31 +0100

    add foreign key from model in countiesApp to directoryApp

--- countiesApp/models.py
+++ countiesApp/models.py
@@ -4,8 +4,10 @@ from django.conf import settings
 # Create your models here.
 
 class County(models.Model):
-    county_name = models.CharField(max_length=200, db_index=True)
-    county_code = models.CharField(max_length=5, db_index=True)
+	class Meta:
+		verbose_name_plural = "Counties"
+	county_name = models.CharField(max_length=200, db_index=True)
+	county_code = models.CharField(max_length=5, db_index=True)
 
-    def __str__(self):
-    	return self.county_name
+	def __str__(self):
+		return self.county_name
